a64ca0759fe24bd423925d6f0d4e6da5f83e6342
AzeezDa <azeez.daoud3@gmail.com>
2025-12-01 14:27:04 +0100

    Add clock in tmux, add formatting options for haskell, markdown, remove texpresso

--- nvim/lua/plugins/texpresso.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "let-def/texpresso.vim",
-    config = function()
-        require('texpresso').texpresso_path = "/home/azeez/apps/texpresso/build/texpresso";
-        vim.keymap.set('n', '<leader>tex', "<cmd>TeXpresso % <CR>")
-    end
-}
